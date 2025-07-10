dd9b349f547cd202331facc72756da8ca8f8c46f
seo-inst <innerpeace.deepi@gmail.com>
2025-07-10 17:55:44 +0900

    feat :  factory , reflection api 적용

--- webstudy12-MVC-Singleton-Command-Factory-inst/src/main/java/org/kosa/myproject/controller/FrontControllerServletVer5.java
+++ webstudy12-MVC-Singleton-Command-Factory-inst/src/main/java/org/kosa/myproject/controller/FrontControllerServletVer5.java
@@ -9,10 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * Ver 4 : MVC + Singleton + Front Controller + Command Design Pattern
+ * Ver 5 : MVC + Singleton + Front Controller + Command + Factory Design Pattern
  */
-@WebServlet("/FrontControllerServletVer4")
-public class FrontControllerServletVer4 extends HttpServlet {
+@WebServlet("/FrontControllerServletVer5")
+public class FrontControllerServletVer5 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doDispatch(HttpServletRequest request, HttpServletResponse response)
@@ -21,20 +21,10 @@ public class FrontControllerServletVer4 extends HttpServlet {
 			// 공통 작업
 			// client 요청 분석
 			String command = request.getParameter("command");
-			// 추상화된 인터페이스 타입
-			Controller controller = null;
-			if (command.equals("findbyid")) {
-				controller = new FindCustomerByIdController();
-			}else if(command.equals("registerCustomer")) {
-				controller = new RegisterCustomerController();
-			}
-			// Command Pattern 적용 (캡슐화로 인해) 으로 표준화된 단일한 방식으로 
-			// 다양한 컨트롤러들을 실행시켜 요청을 처리할 수 있다 
-			String path = controller.handleRequest(request, response);
-			// Controller 영역에서 View 로 제어를 이동하는 방식 : forward 와 redirect 
-			// forward 이동 방식 : request와 response가 유지되지면서 이동하는 방식
-			//                                웹 컨테이너 상에서 이동하여 응답되는 방식 , 정보 조회용도 
-			// 특징 : 기존 request가 유지되므로 새로고침시 재동작되는 특성 -> 등록 등 재동작되면 안되는 업무는 적합x 
+			// 컨트롤러 객체 생성을 전담하는  Factory 객체인 HandlerMapping 을 이용해 
+			// 적절한 컨트롤러를 반환받는다 
+			Controller controller = HandlerMapping.getInstance().create(command);		
+			String path = controller.handleRequest(request, response);			
 			if(path.trim().startsWith("redirect:")) {
 					response.sendRedirect(path.trim().substring(9));
 			}else {				
---
+++ webstudy12-MVC-Singleton-Command-Factory-inst/src/test/java/org/kosa/myproject/test/RedirectUnitTest.java
@@ -0,0 +1,11 @@
+package org.kosa.myproject.test;
+
+public class RedirectUnitTest {
+	public static void main(String[] args) {
+		String url = "  redirect:register-result.jsp ";
+		// 서두 prefix 가 redirect 로 시작되는 가? 
+		System.out.println(url.trim().startsWith("redirect:"));
+		// trim() : 양여백을 제거 
+		System.out.println(url.trim().substring(9));
+	}
+}
